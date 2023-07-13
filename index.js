ca8c81537a8e6c5e5e8a218c6445048805402b20
Suhaib Janjua <suhaib.janjua@amigo-software.com>
2023-07-13 15:39:33 +0500

    packaged it for client side usage

--- index.js
+++ index.js
@@ -1,38 +1,66 @@
-function exportFromMatTable(filename) {
-    const rows = Array.from(document.querySelectorAll('mat-table mat-row'));
+/*
+ * JS Logger
+ * Lightweight JavaScript Logger for the browser and node.
+ * It is a perfect logger that supports all browsers.
+ * It allows to print color logs with pre-defined 5 levels of logging
+ * It has a debug mode in which you can print logs during dev and then set it to false to avoid printing confidential logs during production
+ * Website: https://github.com/suhaibjanjua/js-logger
+ * Copyright: (c) 2019 Suhaib Janjua
+ * License: MIT
+ */
+try {
 
-    // Extract table headers and remove commas
-    const headers = Array.from(document.querySelectorAll('mat-table mat-header-row mat-header-cell')).map(cell => cell.innerText.replace(/,/g, '').trim());
+    var utc = function utc(now) {
+        const datestring = now.toDateString() + " " + ("0" + now.getHours()).slice(-2) + ":" + ("0" + now.getMinutes()).slice(-2) + ":" + ("0" + now.getSeconds()).slice(-2);
+        return datestring;
+    };
 
-    // Extract table data and remove commas from cell values
-    const data = rows.map(row => {
-        return Array.from(row.querySelectorAll('mat-cell')).map(cell => cell.innerText.replace(/,/g, '').trim());
-    });
+    var exportFromMatTable = function exportFromMatTable(matTableSelector, filename) {
 
-    // Create a row for the filename
-    const filenameRow = Array(headers.length).fill('');
-    const filenameRowIndex = Math.floor(filenameRow.length / 2); // Center column index
-    filenameRow[filenameRowIndex] = filename;
+        const table = document.querySelector(matTableSelector);
+        if (!table) {
+            console.error(`Table element "${matTableSelector}" not found.`);
+            return;
+        }
 
-    // Convert to CSV format
-    let csv = '';
-    csv += filenameRow.join(',') + '\n';
-    csv += headers.join(',') + '\n';
-    csv += data.map(row => row.join(',')).join('\n');
+        if (!filename) { filename = "Export-From-MatTable"; }
 
-    // Create a CSV file
-    const csvBlob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
-    const csvURL = URL.createObjectURL(csvBlob);
+        const rows = Array.from(table.querySelectorAll('mat-row'));
 
-    // Trigger file download
-    const downloadLink = document.createElement('a');
-    downloadLink.href = csvURL;
-    downloadLink.download = `${filename}.csv`;
-    downloadLink.style.display = 'none';
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-    document.body.removeChild(downloadLink);
+        // Extract table headers and remove commas
+        const headers = Array.from(table.querySelectorAll('mat-header-row mat-header-cell')).map(cell => cell.innerText.replace(/,/g, '').trim());
+
+        // Extract table data and remove commas from cell values
+        const data = rows.map(row => {
+            return Array.from(row.querySelectorAll('mat-cell')).map(cell => cell.innerText.replace(/,/g, '').trim());
+        });
+
+        // Create a row for the filename
+        const filenameRow = Array(headers.length).fill('');
+        const filenameRowIndex = Math.floor(filenameRow.length / 2); // Center column index
+        filenameRow[filenameRowIndex] = filename;
+
+        // Convert to CSV format
+        let csv = '';
+        csv += filenameRow.join(',') + '\n';
+        csv += headers.join(',') + '\n';
+        csv += data.map(row => row.join(',')).join('\n');
+
+        // Create a CSV file
+        const csvBlob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+        const csvURL = URL.createObjectURL(csvBlob);
+
+        // Trigger file download
+        const downloadLink = document.createElement('a');
+        downloadLink.href = csvURL;
+        downloadLink.download = `${filename + "-" + utc(new Date())}.csv`;
+        downloadLink.style.display = 'none';
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    }
+
+
+} catch (err) {
+    console.log("Please ignore it...", err);
 }
-
-
-module.exports = exportFromMatTable;
--- test.js
+++
@@ -1,3 +0,0 @@
-const exportFromMatTable = require('./index');
-
-exportFromMatTable("ExportFromMatTable");
